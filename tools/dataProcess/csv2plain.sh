01a74173412af8c548a90a9e212d30d738675e18
Huanle Zhang <zhanghuanle1342@gmail.com>
2018-03-21 16:41:39 -0700

    csv2plain.sh add t1,t2,num

--- tools/dataProcess/csv2plain.sh
+++ tools/dataProcess/csv2plain.sh
@@ -5,12 +5,21 @@
 # Huanle Zhang at UC Davis
 # www.huanlezhang.com
 
+myFormat=
+myInputFile=
+myVerbose=0
+
 usage (){
 cat <<EOF
 Usage: csv2plain.sh 
-        --format format_string (t1,t2,r)
+        --format format_string (t1,t2,ip,num,raw)
+                t1,t2: combine to a timestamp 
+                ip: ip numeral to string
+                num: convert bytes to number
+                raw: keep unchanged
         --input input_file
-E.g., csv2plan.sh --format "t1,t2,r" --input file.csv 
+        --verbose
+E.g., csv2plan.sh --format "t1,t2,num" --input file.csv 
 EOF
 }
 
@@ -20,7 +29,77 @@ argumentProcess (){
         exit 
     fi 
 
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+        case $key in
+        --format) 
+            shift
+            myFormat="$1"
+            shift 
+            ;;
+        --input)
+            shift
+            myInputFile="$1"
+            shift 
+            ;;
+        --verbose) 
+            myVerbose=1
+            shift 
+            ;;
+        *) # unknown arguments
+                echo "*** Error unknown argument"
+                echo -e "\t $1"
+                exit
+            ;;
+        esac
+    done
 
+    # check arguments
+    if [[ -z "$myFormat" || -z "$myInputFile" ]]; then
+        echo "*** Error empty argument"
+        echo "--format $myFormat"
+        echo "--input $myInputFile"
+        exit
+    fi
+
+    # display arguments
+    if [[ $myVerbose -eq 1 ]]; then
+        echo "Arguments: "
+        echo -e "\t --format $myFormat"
+        echo -e "\t --input $myInputFile"
+        echo -e "\t --verbose True"
+    fi
 }
 
+dataProcess (){
+    case $myFormat in
+    "t1,t2,num")
+        
+        awk -F "," '
+            {
+                printf "%d,", (1000000*$1+$2);
+                
+                split($3, arr, "-");
+                num=(255**3)*arr[1]+(255**2)*arr[2]+(255**1)*arr[3]+(255**0)*arr[4];
+                print num 
+            }
+        ' $myInputFile > ${myInputFile}.plain
+        ;;
+    *) # unknown
+        echo "*** Error unknown argument:"
+        echo -e "\t $myFormat"
+        exit 
+        ;;
+    esac 
+}
+
+cleanup (){
+    if [[ $myVerbose -eq 0 ]]; then
+        :
+    fi
+}
+
+# --- main starts here ---
 argumentProcess $@
+dataProcess
+cleanup
